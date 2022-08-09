a704c03e7ec29c68192ac8b199d18b7526a1e035
tdoehmen <till.doehmen@web.de>
2022-08-09 10:50:21 +0200

    simplified backend fixtures

--- python/tests/fixtures/backend_fixtures.py
+++ python/tests/fixtures/backend_fixtures.py
@@ -21,5 +21,5 @@ with open('tests/fixtures/backend_fixtures.json', 'r') as json_file:
     backend_fixtures = json.load(json_file)
 
 @pytest.fixture
-def response_get_feature_group():
-    return backend_fixtures["get_feature_group"]["response"]
+def backend_fixtures():
+    return backend_fixtures
